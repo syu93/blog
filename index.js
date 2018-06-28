f27a33f6c5590cf608591cadebefea93366ac4aa
Syu93 <herve.tutuaku@gmail.com>
2018-06-28 10:48:04 +0200

    [feature] start the API server

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const server = require('./server');
 
 // Launch the server
 const app = server();
+
+console.log(`[Server] Listening on port ${app.settings.port} ...`);
+const httpd = app.listen(app.settings.port);
