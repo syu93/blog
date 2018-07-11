1c58d0efa53fd526fa44b190af568f4b42a99e1a
Syu93 <herve.tutuaku@gmail.com>
2018-07-11 03:33:31 +0200

    [feature] fix CORS

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 module.exports = (app) => {
 
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", app.settings.allowedOrigins);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
   });
 
